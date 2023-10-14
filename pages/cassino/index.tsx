15a2f25fe14197818f70681441ade2a9429ae70f
juntbr <victorhugoschneiderr@gmail.com>
2023-10-14 01:44:48 -0300

    fix

--- pages/cassino/index.tsx
+++ pages/cassino/index.tsx
@@ -3,27 +3,27 @@ import axios from 'axios'
 import { GetServerSideProps } from 'next'
 
 export const dynamic = 'force-static'
-export const fetchCache = 'auto'
-
+export const runtime = 'nodejs'
 
 function Cassino({ games }) {
-  console.log({games})
+  console.log({ games })
   return <NotAuthenticatedCasino isLive={false} />
 }
 
 export default Cassino
 
-
-
-export const getServerSideProps: GetServerSideProps = async ({locale, res, req}) => {
+export const getServerSideProps: GetServerSideProps = async ({
+  locale,
+  res,
+  req,
+}) => {
   const { data } = await axios.get(
     'https://lendasbet.nwacdn.com/v1/casino/groups/TESTELOBBY?fields=id,name,games&expand=games',
   )
 
-
   res.setHeader(
     'Cache-Control',
-    'public, s-maxage=10, stale-while-revalidate=59'
+    'public, s-maxage=10, stale-while-revalidate=59',
   )
   return {
     props: {
